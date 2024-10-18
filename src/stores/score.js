ca3e91c063e1509b8d6e7149bbd383b17d4682c7
thisisal1ev <aaalievvv1@gmail.com>
2024-10-18 16:10:19 +0500

    the application logic has been completed

--- src/stores/score.js
+++ src/stores/score.js
@@ -1,7 +1,54 @@
 import { defineStore } from 'pinia'
 
+const baseLevelScore = 25
+
+const levels = new Array(20)
+	.fill(0)
+	.map((_item, i) => baseLevelScore * Math.pow(2, i))
+
+const levelScores = levels.map((_item, level) => {
+	let sum = 0
+
+	for (let [index, value] of levels.entries()) {
+		if (index >= level) {
+			return sum + value
+		}
+		sum += value
+	}
+
+	return sum
+})
+
+function computedLevelByScore(score) {
+	for (let [index, value] of levelScores.entries()) {
+		if (score <= value) {
+			return {
+				level: index,
+				value: levels[index],
+			}
+		}
+	}
+}
+
 export const useScoreStore = defineStore('score', {
-  state: () => ({
-    score: 0,
-  }),
+	state: () => ({
+		score: 0,
+	}),
+	getters: {
+		level: state => computedLevelByScore(state.score),
+		currentScore(state) {
+			if (this.level.level === 0) {
+				return state.score
+			}
+			return state.score - levelScores[this.level.level - 1]
+		},
+	},
+	actions: {
+		add(score = 1) {
+			this.score += score
+		},
+		setScore(score) {
+			this.score = score
+		},
+	},
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,56 @@
 <template>
-  <div class="game-container">
-    <ScoreProgress />
-    <div class="header">
-      <img src="../assets/coin.png" alt="coin" />
-      <h2 class="score" id="score">42</h2>
-    </div>
-    <div class="circle">
-      <img class="" @click="increment" ref="img" id="circle" src="../assets/frog.png" />
-    </div>
-  </div>
+	<div class="game-container">
+		<ScoreProgress />
+		<div class="header">
+			<img src="../assets/coin.png" alt="coin" />
+			<h2 class="score" id="score">{{ store.score }}</h2>
+		</div>
+		<div class="circle">
+			<img @click="increment" ref="img" id="circle" :src="imgSrc" />
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import ScoreProgress from '@/components/ScoreProgress.vue'
+import { useScoreStore } from '@/stores/score'
+import { computed, ref } from 'vue'
 
+import frog from '@/assets/frog.png'
+import lizard from '@/assets/lizzard.png'
+
+const store = useScoreStore()
 const img = ref(null)
+const imgSrc = computed(() => (store.score > 50 ? lizard : frog))
 
 function increment(event) {
-  const rect = event.target.getBoundingClientRect()
+	store.add()
+	const rect = event.target.getBoundingClientRect()
 
-  const offfsetX = event.clientX - rect.left - rect.width / 2
-  const offfsetY = event.clientY - rect.top - rect.height / 2
+	const offfsetX = event.clientX - rect.left - rect.width / 2
+	const offfsetY = event.clientY - rect.top - rect.height / 2
 
-  const DEG = 40
+	const DEG = 40
 
-  const tiltX = (offfsetY / rect.height) * DEG
-  const tiltY = (offfsetX / rect.width) * -DEG
+	const tiltX = (offfsetY / rect.height) * DEG
+	const tiltY = (offfsetX / rect.width) * -DEG
 
-  img.value.style.setProperty('--tiltX', `${tiltX}deg`)
-  img.value.style.setProperty('--tiltY', `${tiltY}deg`)
+	img.value.style.setProperty('--tiltX', `${tiltX}deg`)
+	img.value.style.setProperty('--tiltY', `${tiltY}deg`)
 
-  setTimeout(() => {
-    img.value.style.setProperty('--tiltX', `0deg`)
-    img.value.style.setProperty('--tiltY', `0deg`)
-  }, 300)
+	setTimeout(() => {
+		img.value.style.setProperty('--tiltX', `0deg`)
+		img.value.style.setProperty('--tiltY', `0deg`)
+	}, 300)
 
-  const plusOne = document.createElement('div')
-  plusOne.classList.add('plus-one')
-  plusOne.textContent = '+1'
-  plusOne.style.left = `${event.clientX - rect.left}px`
-  plusOne.style.top = `${event.clientY - rect.top}px`
+	const plusOne = document.createElement('div')
+	plusOne.classList.add('plus-one')
+	plusOne.textContent = '+1'
+	plusOne.style.left = `${event.clientX - rect.left}px`
+	plusOne.style.top = `${event.clientY - rect.top}px`
 
-  img.value.parentElement.appendChild(plusOne)
+	img.value.parentElement.appendChild(plusOne)
 
-  setTimeout(() => plusOne.remove(), 2000)
+	setTimeout(() => plusOne.remove(), 2000)
 }
 </script>
