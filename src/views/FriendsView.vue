7f04feee8c4138e2bd5f6813374a8bd55dbfb3bd
thisisal1ev <aaalievvv1@gmail.com>
2024-10-26 13:16:37 +0500

    fix(#1)

--- src/views/FriendsView.vue
+++ src/views/FriendsView.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="text-content">
-    <h1>Your Friends</h1>
+	<div class="text-content">
+		<h1>Your Friends</h1>
 
-    <div class="center">
-      <button class="referral" @click="copy">{{ referralText }}</button>
-    </div>
+		<div class="center">
+			<button class="referral" @click="copy">{{ referralText }}</button>
+		</div>
 
-    <h3 v-if="friends.length === 0">No friends yet</h3>
+		<h3 v-if="friends.length === 0">No friends yet</h3>
 
-    <ul class="list">
-      <li class="list-item" v-for="friend in friends" :key="friend.id">
-        {{ friend.name }}
-        <span class="list-btn done">50</span>
-      </li>
-    </ul>
-  </div>
+		<ul class="list">
+			<li class="list-item" v-for="friend in friends" :key="friend.id">
+				{{ friend.name }}
+				<span class="list-btn done">{{ friend.amount }}</span>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script setup>
@@ -28,16 +28,16 @@ const { user } = useTelegram()
 const referralText = ref('Your referral')
 
 const friends = computed(() =>
-  Object.keys(app.user.friends).map((id) => ({
-    id,
-    name: app.user.friends[id],
-  }))
+	Object.keys(app.user.friends).map(id => ({
+		id,
+		name: app.user.friends[id],
+	}))
 )
 
 function copy() {
-  navigator.clipboard.writeText(
-    'https://t.me/LizardClickerProBot?start=' + user?.id
-  )
-  referralText.value = 'Copied!'
+	navigator.clipboard.writeText(
+		'https://t.me/LizardClickerProBot?start=' + user?.id
+	)
+	referralText.value = 'Copied!'
 }
 </script>
